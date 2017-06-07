8a9f57481eb585db911f4e27771d192d7f0c7db2
waldstein <wangbaohua2009@163.com>
2017-06-07 17:52:44 +0800

    Add MajorityElement, DeleteNodeInLinkedList

---
+++ src/com/leetcode/problem/MajorityElement.java
@@ -0,0 +1,24 @@
+package com.leetcode.problem;
+
+/**
+ * Created by baohuaw on 2017/6/7.
+ */
+public class MajorityElement {
+    //voting algorithm, linear time
+    public int majorityElement(int[] nums) {
+        int result = 0, count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                result = nums[i];
+                count = 1;
+            } else if (result == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return result;
+    }
+}
